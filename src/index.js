7c5264ef559f2457f1527f050bb8d9dc88d312b7
atassielli <anthonytassielli@gmail.com>
2022-10-03 21:02:36 -0700

    Create button indicators for header buttons
    style updates including indicators for which page of the website is currently open

--- src/index.js
+++ src/index.js
@@ -10,36 +10,59 @@ pageLoader();
 home();
 
 let currentPage = "home"
-
 let homeButton = document.querySelector('.homeButton');
+let menuButton = document.querySelector('.menuButton');
+let contactButton = document.querySelector('.contactButton');
+changeButton();
+
 homeButton.addEventListener('click', () => {
     if (currentPage === "home") {
         return 
     } else {
         clearPage();
         currentPage = "home";
+        changeButton();
         home();
     }
 })
 
-let contactButton = document.querySelector('.contactButton');
 contactButton.addEventListener('click', () => {
     if (currentPage === 'contact') {
         return
     } else {
         clearPage();
         currentPage = 'contact';
+        changeButton();
         contact();
     }
 })
 
-let menuButton = document.querySelector('.menuButton');
 menuButton.addEventListener('click', () => {
     if (currentPage === 'menu') {
         return
     } else {
         clearPage();
         currentPage = 'menu';
+        changeButton();
         menu();
     }
 })
+
+function changeButton() {
+    if (currentPage === 'home') {
+        homeButton.style.cssText = "background-color: white; color: black";
+        menuButton.style.cssText = "background-color: black; color: white";
+        contactButton.style.cssText = "background-color: black; color: white";
+        return;
+    } else if (currentPage === 'menu') {
+        homeButton.style.cssText = "background-color: black; color: white";
+        menuButton.style.cssText = "background-color: white; color: black";
+        contactButton.style.cssText = "background-color: black; color: white";
+        return;
+    } else if (currentPage === 'contact') {
+        homeButton.style.cssText = "background-color: black; color: white";
+        menuButton.style.cssText = "background-color: black; color: white";
+        contactButton.style.cssText = "background-color: white; color: black";
+        return;
+    }
+}
